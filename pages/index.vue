b7b19919a2eaea562e8b3319e9678780df8a0883
German Khokhlov <lkg0dzre@gmail.com>
2022-12-14 18:01:24 +0530

    added pages

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nemo ad eaque, praesentium animi odio neque quam mollitia corrupti adipisci sequi. Laudantium dolorem, libero assumenda recusandae aperiam dolores eveniet molestiae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nemo ad eaque, praesentium animi odio neque quam mollitia corrupti adipisci sequi. Laudantium dolorem, libero assumenda recusandae aperiam dolores eveniet molestiae.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+
+
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+
+  p {
+    margin: 20px 0;
+  }
+</style>
