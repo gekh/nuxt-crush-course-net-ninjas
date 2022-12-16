4e2af4d3001eac560e2fb66a536d0ae0e8ccc9c8
German Khokhlov <lkg0dzre@gmail.com>
2022-12-16 21:49:59 +0530

    added a call from server side api to outer api

--- pages/about.vue
+++ pages/about.vue
@@ -8,10 +8,7 @@
 </template>
 
 <script setup>
-  const { data } = await useFetch('/api/ninja?name=mario', {
-    method: 'post',
-    body: { age: 30 },
-  });
+  const { data } = await useFetch('/api/ninja');
 </script>
 
 <style scoped>
--- server/api/ninja.js
+++ server/api/ninja.js
@@ -1,17 +1,9 @@
 export default defineEventHandler(async (event) => {
 
-  // handle event param
-  const { name } = getQuery(event);
-
-  // handle post data
-  const { age } = await readBody();
-
   // api call with private key
   const API_KEY = 'uz0cP5vThfrP75nS7CNQyOHMkDRpodPQxz6sypQR';
   const { data } = await $fetch('https://api.currencyapi.com/v3/latest?apikey=' + API_KEY);
 
-  return {
-    message: `Hello, ${name}! You are ${age} years old.`
-  };
+  return data;
 
 });
